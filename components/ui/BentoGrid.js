aec6c70e3f5abd9f1c450ca99521c83eab32fe45
Priya Panwar <priya24@iitk.ac.in>
2025-07-24 14:27:50 +0530

    Update BentoGrid.js

--- components/ui/BentoGrid.js
+++ components/ui/BentoGrid.js
@@ -7,7 +7,7 @@ export function BentoGrid({ className, children }) {
   return <div className={`bento-grid-wrapper ${className || ""}`}>{children}</div>;
 }
 
-export function BentoGridItem({ className, title, description, header, icon }) {
+export function BentoGridItem({ className, title, description, header}) {
   return (
     <motion.div
       initial={{ opacity: 0, y: 30 }}
@@ -18,7 +18,7 @@ export function BentoGridItem({ className, title, description, header, icon }) {
     >
       
       {header && <div className="bento-item-header">{header}</div>}
-      <div className="bento-item-icon">{icon}</div>
+      // <div className="bento-item-icon">{icon}</div>
       <div className="bento-item-title">{title}</div>
       <div className="bento-item-description">{description}</div>
     </motion.div>
