0a9c88323bab82900e2ec0e906c8ed503253be6c
vjhanani <jhananiev9@gmail.com>
2025-08-02 12:52:21 +0530

    Updated about page

--- components/About.js
+++ components/About.js
@@ -34,7 +34,9 @@ export default function About() {
       <h1 className="about-heading">ABOUT US</h1>
       <p className="about-description">
         The International Relations Wing of the Academics and Career Council at IIT Kanpur is a dedicated team of bright and ambitious students committed to fostering and nurturing relationships between IITK and its international counterparts. Working closely with the Office of International Relations (OIR), our mission is to encourage talented young minds to explore their areas of interest, enhance their skills, and develop a global perspective through semester exchange programs and research internships with top-notch international institutes.
+      <br />
         Our team works tirelessly to maintain a spirit of collaboration, excitement, and a broad perspective in the interest of campus community. We are dedicated to enriching the IITK community and promoting global academic exchange.
+     
       </p>
     </div>
   );
